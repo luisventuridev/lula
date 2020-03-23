c22c098f487b8bf9712f9b689f408da39ae3e96b
Luis Venturi <hello@luisventuri.com>
2020-03-23 08:39:33 -0500

    a

--- gulpfile.js
+++ gulpfile.js
@@ -1,1 +1,24 @@
-const gulp = require('gulp');
+const gulp = require('gulp'),
+    sass = require('gulp-sass'),
+    autoprefixer = require('gulp-autoprefixer'),
+    pug = require('gulp-pug'),
+    watch = require('gulp-watch')
+
+
+
+gulp.task('estilo', () =>
+    gulp.src('./src/scss/styles.scss')
+    .pipe(sass({
+        outputStyle: "compressed" 
+    }))
+    .pipe(autoprefixer({
+        version: ['last 3 browsers']
+    }))
+    .pipe(gulp.dest('./dist/css/styles.css'))
+
+);
+
+
+gulp.task('default', () => {
+    gulp.watch('./src/scss/styles.scss', ['estilo']);
+});
